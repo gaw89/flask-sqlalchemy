0258dc0c296fdee46ca5449dc6e91c359516c75e
Gideon Whitehead <gideon.whitehead@uphs.upenn.edu>
2018-11-15 07:50:09 -0500

    Update __init__.py

--- flask_sqlalchemy/__init__.py
+++ flask_sqlalchemy/__init__.py
@@ -564,7 +564,7 @@ class _EngineConnector(object):
             config_engine_opts = self._app.config.get('SQLALCHEMY_ENGINE_OPTS', {})
             options.update(config_engine_opts)
                 
-            self._engine = rv = sqlalchemy.create_engine(info, **options)
+            self._engine = rv = sqlalchemy.create_engine(info, pool_pre_ping=True, **options)
             if _record_queries(self._app):
                 _EngineDebuggingSignalEvents(self._engine,
                                              self._app.import_name).register()
